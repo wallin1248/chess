647005929fb6bf2ebf2cc2592da53044ecd090a0
wallin1248 <dallinwise@gmail.com>
2026-01-27 20:07:36 -0700

    Added equals, hash and tostring

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import static java.util.Objects.hash;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -36,4 +38,41 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() { return this.promotionPiece; }
+
+    /**
+     * Override the equals command for this function specifically.
+     *
+     * @return true if both moves start and end in the same positions and have the same promotions
+     *
+     */
+    @Override
+    public boolean equals(Object otherMove) {
+        // If both are the same reference, return true
+        if (this == otherMove) { return true; }
+
+        // Check if both have the same color and piece type
+        ChessMove chessMove = (ChessMove) otherMove;
+        return this.startPosition == ((ChessMove) otherMove).startPosition
+                && this.endPosition == chessMove.endPosition
+                && this.promotionPiece == chessMove.promotionPiece;
+    }
+
+    /**
+     * Override the hashcode command
+     * */
+    @Override
+    public int hashCode() {
+        return hash(this.startPosition, this.endPosition, this.promotionPiece);
+    }
+
+    /**
+     * Create the print method for this object.
+     */
+    @Override
+    public String toString() {
+        return "ChessMove{Start: " + this.startPosition
+                + ", End: " + this.endPosition
+                + ", Promote:" + this.promotionPiece
+                + "}";
+    }
 }
